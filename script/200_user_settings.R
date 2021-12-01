36d55af21388e860f966f07c20e40f680e423211
Alexander Haeusser <alexander-haeusser@gmx.de>
2021-12-01 16:28:21 +0100

    Modified and cleaned user_settings

--- script/200_user_settings.R
+++ script/200_user_settings.R
@@ -8,97 +8,88 @@ info(
 
 # User settings ===============================================================
 
-# set_series <- "M23824"
-# set_series <- "M23898"
-# set_series <- "M30884"
-# set_series <- "M35105"
-# set_series <- "M35161"
-# set_series <- "M14395"
-# set_series <- "M13450"
-# set_series <- "M18491"
-# set_series <- "M30851"
-# set_series <- "M45533"
-# set_series <- "M15320"
-# set_series <- "M5187"
-set_series <- "M5535"
-# set_series <- "M5937"
-# set_series <- "M34487"
-# set_series <- "M10960"
-# set_series <- "M12123"
-# set_series <- "M12717"
-# set_series <- "M13027"
-# set_series <- "M23117"
-# set_series <- "M23679"
-# set_series <- "M3697"
+# Identifier of data set ------------------------------------------------------
 
+input_file <- "data/elec_price.rds"
 
-test_run <- FALSE
+series_id <- "series"            # unique identifier for time series
+series_id <- "bidding_zone"
+value_id <- "value"              # identifier for measurement column
+index_id <- "time"               # identifier for time index column
+
+context <- list(
+  series_id = series_id,
+  value_id = value_id,
+  index_id = index_id
+)
+
+# Setup for test run ----------------------------------------------------------
+
+test_run <- TRUE                # test run or full run?
+test_seed <- 123                # for reproducibility
+n_test_splits <- 10             # number of random splits for testing
+n_test_series <- 4              # number of random series for testing
 
 # Setup for time series cross validation --------------------------------------
 
+type <- "first"                 # type of initial training window
+value <- 2400                   # size for initial training window
+n_ahead <- 24                   # size for testing window (forecast horizon)
+n_skip <- 23                    # skip 23 observations
+n_lag <- 0                      # no lag
+mode <- "slide"                 # fixed window approach
+exceed <- FALSE                 # out-of-sample forecasts?
 
-data <- M4_monthly_data %>%
-  filter(series_id == set_series)
+# Evaluation of forecast accuracy ---------------------------------------------
 
-n_obs <- nrow(data)
-n_init <- floor(n_obs * 0.8)
-n_ahead <- 18
-mode <- "slide"
-n_skip <- 0
-n_lag <- 0
-period <- c(12)
-
-# n_init <- 2400               # size for training window
-# n_ahead <- 24                # size for testing window (forecast horizon)
-# mode <- "slide"              # fixed window approach
-# n_skip <- 23                 # skip 23 observations
-# n_lag <- 0                   # no lag
+set_metric <- "rMAE"            # Accuracy metric for overall summary
+benchmark <- "SNAIVE"           # Benchmark method for rMAE
 
 # Modeling --------------------------------------------------------------------
 
-# period <- c(24, 168)         # seasonal periods
-outlier <- FALSE              # outlier adjustment
-# shift <- 200                 # constant shift to avoid negative values (DSHW)
-
-# models <- c(
-#   "ESN",
-#   "SNAIVE",
-#   "SNAIVE2",
-#   "STL-NAIVE",
-#   "STL-ARIMA",
-#   "STL-ETS",
-#   "EXPERT",
-#   "DHR-ARIMA",
-#   "DSHW",
-#   "TBATS",
-#   "ELM",
-#   "FASSTER"
-# )
+period <- c(24, 168)            # seasonal periods
+outlier <- TRUE                 # outlier adjustment
+shift <- 200                    # constant shift to avoid negative values (DSHW)
 
 models <- c(
-  "ESN",
-  "ARIMA",
+  # "NAIVE",
+  # "DRIFT",
+  "SNAIVE",
+  "SNAIVE2",
+  # "MEAN",
+  # "TSLM",
+  # "ARIMA",
+  # "ETS",
+  "DSHW",
+  # "TBATS",
+  # "DHR-ARIMA",
+  "STL-NAIVE",
   "STL-ARIMA",
-  "STL-NAIVE"
+  "STL-ETS",
+  # "ELM",
+  # "FASSTER",
+  "EXPERT"
+  # "ESN"
   )
 
 # Echo State Networks ---------------------------------------------------------
 
-lags = list(c(1:6, 12))
-fourier = list(c(12), c(6))
-xreg = NULL
-dy = 1
-dx = 0
-inf_crit = "aic"
-n_seed = 42
-alpha = 0.7
-rho = 1
-density = 0.1
-scale_win = 0.5
-scale_wres = 0.5
-scale_inputs = c(-1, 1)
+# lags = list(c(1:6, 12))
+# fourier = NULL
+# xreg = NULL
+# dy = NULL
+# dx = 0
+# inf_crit = "aic"
+# n_seed = 42
+# alpha <- c(0.5, 0.7, 0.9, 1)
+# rho <- c(0.5, 1)
+# n_states <- 50
+# density = 0.1
+# scale_win = 0.5
+# scale_wres = 0.5
+# scale_inputs = c(-1, 1)
 
-# Create meta_object ----------------------------------------------------------
+# Save user_settings as list --------------------------------------------------
 
 user_settings <- mget(ls())
 
