dc38d35cb91ec0cb12feaa8a4ef5e445a177df6c
Alexander Haeusser <alexander-haeusser@gmx.de>
2021-12-01 16:32:22 +0100

    Renamed and modified

---
+++ script/408_model_arima.R
@@ -0,0 +1,55 @@
+
+if ("ARIMA" %in% models) {
+  
+  info(
+    logger = logger,
+    message = "START  script/408_model_arima.R"
+  )
+  
+  .start <- Sys.time()
+  
+  # ARIMA =====================================================================
+  
+  # Train and forecast models -------------------------------------------------
+  
+  with_progress({
+    p <- progressor(steps = nrow(split_frame))
+    future_arima <- future_map_dfr(
+      .x = seq_len(nrow(split_frame)),
+      .f = ~{
+        p()
+        # Slice training data according to split
+        train_frame <- slice_train(
+          main = main_frame,
+          split = split_frame[.x, ],
+          context = context)
+        # Convert to tsibble, model and forecast
+        fable_frame <- train_frame %>%
+          as_tsibble(
+            index = !!sym(index_id),
+            key = c(!!sym(series_id), split)) %>%
+          model("ARIMA" = ARIMA(!!sym(value_id))) %>%
+          forecast(h = n_ahead)
+        # Convert fable to future_frame
+        future_frame <- make_future(
+          fable = fable_frame,
+          context = context
+        )
+      })
+  })
+  
+  # Store forecasts in future_frame -------------------------------------------
+  
+  future_frame[["ARIMA"]] <- future_arima
+  rm(future_arima)
+  
+  info(
+    logger = logger,
+    message = paste0(
+      "FINISH script/408_model_arima.R",
+      "\n",
+      log_time(start = .start),
+      "\n"
+    )
+  )
+}
