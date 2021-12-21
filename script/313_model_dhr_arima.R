4161a5fe91d0daea1078f49bf0e9f8e72e624030
Alexander Haeusser <alexander-haeusser@gmx.de>
2021-12-21 17:12:24 +0100

    Debugging: Changed object name periods

--- script/313_model_dhr_arima.R
+++ script/313_model_dhr_arima.R
@@ -24,7 +24,7 @@ if ("DHR-ARIMA" %in% models) {
           as_tsibble(
             index = !!sym(index_id),
             key = c(!!sym(series_id), split)) %>%
-          model("DHR-ARIMA" = ARIMA(!!sym(value_id) ~ fourier(period[1], period[1]/2) + fourier(period[2], period[1]/4) + pdq(d = 0) + PDQ(0, 0, 0))) %>%
+          model("DHR-ARIMA" = ARIMA(!!sym(value_id) ~ fourier(periods[1], periods[1]/2) + fourier(periods[2], periods[1]/4) + pdq(d = 0) + PDQ(0, 0, 0))) %>%
           forecast(h = n_ahead)
         # Convert fable to future_frame
         future_frame <- make_future(
