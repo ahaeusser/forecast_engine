53db9713f8e30fe77afc8ea6fbf70e90e849ea38
Alexander Haeusser <alexander-haeusser@gmx.de>
2025-08-27 10:15:40 +0200

    Update

--- analysis/script_hyperparameters.R
+++ analysis/script_hyperparameters.R
@@ -20,10 +20,10 @@ options(warn = set_warn)
 plan(multisession)
 
 # Frequency of dataset
-set_freq <- "monthly"
-# set_freq <- "quarterly"
+# set_freq <- "monthly"
+set_freq <- "quarterly"
 # Information criterion
-set_inf_crit <- "hqc"
+set_inf_crit <- "aic"
 # Outlier adjustment
 outlier <- TRUE
 # Test run
