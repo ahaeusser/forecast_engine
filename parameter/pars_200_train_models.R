732f4ad6b33c2617d7902f3e2e469a60e5feb7ce
Alexander Haeusser <alexander-haeusser@gmx.de>
2025-09-05 09:54:34 +0200

    Update

--- parameter/pars_200_train_models.R
+++ parameter/pars_200_train_models.R
@@ -16,7 +16,7 @@ set_inf_crit <- "bic"
 # Outlier adjustment
 outlier <- TRUE
 # Test run
-test_run <- TRUE
+test_run <- FALSE
 
 # Directory and file names, forecast horizon and period
 if (set_freq == "monthly") {
