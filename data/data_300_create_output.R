7b6e8e42f90fd8177f12ebc5ceb6173aedca8ee4
Alexander Haeusser <alexander-haeusser@gmx.de>
2025-09-07 13:41:57 +0200

    Update

--- data/data_300_create_output.R
+++ data/data_300_create_output.R
@@ -3,104 +3,126 @@ library(tidyverse)
 library(tscv)
 library(feasts)
 library(tsibble)
+library(gt)
 
 # Load data ===================================================================
 
-### TO DO #####################################################################
-# main_mth_total
-# meta_mth_total
-# meta_mth_forecast
-# meta_mth_parameter
+# Monthly data ----------------------------------------------------------------
 
-# Main files ------------------------------------------------------------------
+main_mth_total <- readRDS(file = "data/main_mth_total.rds")
+meta_mth_total <- readRDS(file = "data/meta_mth_total.rds")
+meta_mth_parameter <- readRDS(file = "data/meta_mth_parameter.rds")
+meta_mth_forecast <- readRDS(file = "data/meta_mth_forecast.rds")
 
-# Directory and file names
-files_main_mth <- list(
-  "data/main_mth_total.rds",
-  "data/main_mth_parameter.rds",
-  "data/main_mth_forecast.rds"
-)
+# Quarterly data --------------------------------------------------------------
 
-# Read rds-files and combine data frames row-wise
-main_frame_mth <- bind_rows(lapply(files_main_mth, readRDS))
+main_qtr_total <- readRDS(file = "data/main_qtr_total.rds")
+meta_qtr_total <- readRDS(file = "data/meta_qtr_total.rds")
+meta_qtr_parameter <- readRDS(file = "data/meta_qtr_parameter.rds")
+meta_qtr_forecast <- readRDS(file = "data/meta_qtr_forecast.rds")
 
-# Directory and file names
-files_main_qtr <- list(
-  "data/main_qtr_total.rds",
-  "data/main_qtr_parameter.rds",
-  "data/main_qtr_forecast.rds"
-)
 
-# Read rds-files and combine data frames row-wise
-main_frame_qtr <- bind_rows(lapply(files_main_qtr, readRDS))
+# Estimate STL features (strength of trend and seasonality) ===================
 
-# Meta files ------------------------------------------------------------------
+# Monthly data ----------------------------------------------------------------
 
-# Directory and file names
-files_meta_mth <- list(
-  "data/meta_mth_total.rds",
-  "data/meta_mth_parameter.rds",
-  "data/meta_mth_forecast.rds"
-)
-
-# Read rds-files and combine data frames row-wise
-meta_frame_mth <- bind_rows(lapply(files_meta_mth, readRDS))
-
-# Directory and file names
-files_meta_qtr <- list(
-  "data/meta_qtr_total.rds",
-  "data/meta_qtr_parameter.rds",
-  "data/meta_qtr_forecast.rds"
-)
-
-# Read rds-files and combine data frames row-wise
-meta_frame_qtr <- bind_rows(lapply(files_meta_qtr, readRDS))
-
-# Exploratory data analysis (trend, seasonality, etc.) ========================
-
-# Prepare data
-feature_frame_mth <- main_frame_mth %>%
-  as_tsibble(
-    key = c("series", "dataset"),
-    index = "index") %>%
+feature_frame_mth <- main_mth_total %>%
+  as_tsibble(key = "series", index = "index") %>%
   features(value, feat_stl) %>%
   select(series, trend_strength, seasonal_strength_year)
 
-feature_frame_qtr <- main_frame_qtr %>%
-  as_tsibble(
-    key = c("series", "dataset"),
-    index = "index") %>%
+meta_frame_mth <- left_join(
+  x = meta_mth_total,
+  y = feature_frame_mth,
+  by = "series"
+)
+
+series_mth_pars <- unique(meta_mth_parameter[["series"]])
+series_mth_fcst <- unique(meta_mth_forecast[["series"]])
+
+meta_frame_mth_pars <- meta_frame_mth %>%
+  filter(series %in% series_mth_pars) %>%
+  mutate(dataset = "Parameter")
+
+meta_frame_mth_fcst <- meta_frame_mth %>%
+  filter(series %in% series_mth_fcst) %>%
+  mutate(dataset = "Forecast")
+
+meta_frame_mth <- bind_rows(
+  meta_frame_mth,
+  meta_frame_mth_pars,
+  meta_frame_mth_fcst
+)
+
+# Quarterly data --------------------------------------------------------------
+
+feature_frame_qtr <- main_qtr_total %>%
+  as_tsibble(key = "series", index = "index") %>%
   features(value, feat_stl) %>%
   select(series, trend_strength, seasonal_strength_year)
 
-feature_frame <- bind_rows(
-  feature_frame_mth,
-  feature_frame_qtr
+meta_frame_qtr <- left_join(
+  x = meta_qtr_total,
+  y = feature_frame_qtr,
+  by = "series"
 )
 
+series_qtr_pars <- unique(meta_qtr_parameter[["series"]])
+series_qtr_fcst <- unique(meta_qtr_forecast[["series"]])
 
+meta_frame_qtr_pars <- meta_frame_qtr %>%
+  filter(series %in% series_qtr_pars) %>%
+  mutate(dataset = "Parameter")
 
+meta_frame_qtr_fcst <- meta_frame_qtr %>%
+  filter(series %in% series_qtr_fcst) %>%
+  mutate(dataset = "Forecast")
 
+meta_frame_qtr <- bind_rows(
+  meta_frame_qtr,
+  meta_frame_qtr_pars,
+  meta_frame_qtr_fcst
+)
+
+# Combine datasets
 meta_frame <- bind_rows(
   meta_frame_mth %>% select(-c(start, end)),
   meta_frame_qtr %>% select(-c(start, end))
 )
 
-meta_frame <- left_join(
-  x = meta_frame,
-  y = feature_frame,
-  by = "series"
-)
-
-
 meta_frame <- meta_frame %>%
-  select(-c(category, n_ahead)) %>%
+  select(-n_ahead) %>%
+  rename(
+    "Length" = n_obs,
+    "Trend" = trend_strength,
+    "Season" = seasonal_strength_year) %>%
   pivot_longer(
-    cols = -c(series, freq, dataset),
+    cols = -c(series, category, freq, dataset),
     names_to = "metric",
     values_to = "value") %>%
   arrange(series, freq, metric)
 
+
+# Reorder factor levels of frequency
+meta_frame$freq <- factor(
+  meta_frame$freq,
+  levels = c(
+    "Monthly", 
+    "Quarterly"
+  )
+)
+
+# Reorder factor levels of dataset
+meta_frame$dataset <- factor(
+  meta_frame$dataset,
+  levels = c(
+    "Total", 
+    "Parameter",
+    "Forecast"
+  )
+)
+
+# Estimate summary statistics
 table_meta <- meta_frame %>%
   group_by(freq, metric, dataset) %>%
   summarise(
@@ -110,9 +132,8 @@ table_meta <- meta_frame %>%
     "Median" = round(median(value, na.rm = TRUE), 3),
     "Q75"    = round(quantile(x = value, probs = 0.75), 3),
     "Max"    = round(max(value, na.rm = TRUE), 3),
-    "Std"    = round(sd(value, na.rm = TRUE), 3)) %>%
-  ungroup() %>%
-  arrange(freq, metric, dataset)
+    "Std"    = round(sd(value, na.rm = TRUE), 3),
+    .groups = "drop")
 
 table_meta %>%
   gt() %>%
@@ -123,62 +144,40 @@ table_meta %>%
 
 
 
+
 meta_frame <- meta_frame %>%
-  mutate(across(freq, factor, levels=c("Monthly","Quarterly"))) %>%
-  mutate(across(dataset, factor, levels=c("M4", "Sample"))) %>%
-  mutate(metric = recode(
-    metric, 
-    n_obs = "Number of obs.",
-    seasonal_strength_year = "Seasonal strength",
-    trend_strength = "Trend strength")) 
-
-
-
-
-###
-
-meta_frame_season <- meta_frame %>%
-  filter(freq == "Monthly") %>%
-  filter(metric == "Seasonal strength") %>%
-  arrange(desc(value))
-
-###
-
-
-
-set_freq <- "Monthly"
-
-meta_frame2 <- meta_frame %>%
-  filter(freq == set_freq)
+  filter(value <= 500)
 
 p <- ggplot()
 
-p <- p + geom_histogram(
-  data = meta_frame2,
-  aes(x = value),
-  position = "identity",
-  alpha = 0.5
+p <- p + geom_density(
+  data = meta_frame,
+  aes(x = value, color = dataset, fill = dataset),
+  alpha = 0.05,
+  size = 0.8
 )
 
+# p <- p + scale_x_log10()
+
 p <- p + facet_wrap(
-  vars(dataset, metric),
+  freq ~ metric,
   scales = "free",
   nrow = 2
 )
 
 p <- p + labs(x = "Value")
-p <- p + labs(y = "Absolute frequency")
+p <- p + labs(y = "Density")
 p <- p + theme_tscv()
 
 p
 
 
-figure_name <- paste0("figures/figure_01_data_monthly",".pdf")
+figure_name <- paste0("figure_01_data_monthly",".pdf")
 
 ggsave(
   filename = figure_name,
   width = 17, # 25
-  height = 10, # 10
+  height = 15, # 10
   units = "cm"
 )
 
