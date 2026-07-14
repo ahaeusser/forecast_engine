abbcdff0002d4641c780d2be55235e75847a5f31
Alexander Haeusser <alexander-haeusser@gmx.de>
2026-07-14 20:35:46 +0200

    Added script

---
+++ forecast/fcst_399_model_import.R
@@ -0,0 +1,64 @@
+
+.start <- Sys.time()
+.step <- "399_model_import.R"
+
+if (c("MLP") %in% models) {
+  
+  # Multilayer Perceptron (MLP) ===============================================
+  
+  future_mlp <- import_benchmarks(
+    file = "data/submission-MLP.csv",
+    model = "MLP",
+    main_frame = main_frame,
+    n_ahead = n_ahead
+  )
+  
+  future_frame[["MLP"]] <- future_mlp
+  rm(future_mlp)
+}
+
+if (c("RNN") %in% models) {
+  
+  # Recurrent Neural Networks (RNN) ===========================================
+  future_rnn <- import_benchmarks(
+    file = "data/submission-RNN.csv",
+    model = "RNN",
+    main_frame = main_frame,
+    n_ahead = n_ahead
+  )
+  
+  future_frame[["RNN"]] <- future_rnn
+  rm(future_rnn)
+}
+
+if (c("ES-RNN") %in% models) {
+  
+  # Exponential Smoothing + Recurrent Neural Network (ES-RNN) =================
+  # (winning solution; submission no. 118)
+  
+  future_118 <- import_benchmarks(
+    file = "data/submission-118.csv",
+    model = "ES-RNN",
+    main_frame = main_frame,
+    n_ahead = n_ahead
+  )
+  
+  future_frame[["ES-RNN"]] <- future_118
+  rm(future_118)
+}
+
+# Write log file ============================================================
+
+write_lines(
+  x = log_time(text = .step, start = .start),
+  file = glue("{folder}/{run_name}.txt"),
+  append = TRUE
+)
+
+print(
+  log_time(
+    text = .step, 
+    start = .start,
+    ft_bold = TRUE
+  )
+)
